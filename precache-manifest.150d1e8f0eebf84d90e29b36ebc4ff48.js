023f71e9f8d13ed5f70a138a936f181958041920
“geroalberino“ <gero.alberino3@gmail.ccomssh-keygen -t rsa -C gero.alberino3@gmail.com>
2020-04-04 14:58:22 -0400

    Updates

---
+++ precache-manifest.150d1e8f0eebf84d90e29b36ebc4ff48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf9713f5bd206d388b420f08d5c27be6",
+    "url": "/geroalberino-RoboFriends/index.html"
+  },
+  {
+    "revision": "a3a65d4fcbb730942321",
+    "url": "/geroalberino-RoboFriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "bec36331ce3019229d48",
+    "url": "/geroalberino-RoboFriends/static/css/main.8cc55ca5.chunk.css"
+  },
+  {
+    "revision": "a3a65d4fcbb730942321",
+    "url": "/geroalberino-RoboFriends/static/js/2.43071c07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/geroalberino-RoboFriends/static/js/2.43071c07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bec36331ce3019229d48",
+    "url": "/geroalberino-RoboFriends/static/js/main.239bdbbf.chunk.js"
+  },
+  {
+    "revision": "d518d06e9637924d4ba3",
+    "url": "/geroalberino-RoboFriends/static/js/runtime-main.ad9c9e56.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/geroalberino-RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
